ca39724dd7cded6cea97851353dd326077e26dc9
konstarina <55761341+konstarina@users.noreply.github.com>
2021-06-02 16:15:56 +0300

    add case with time window

--- lib/database/database.ex
+++ lib/database/database.ex
@@ -4,7 +4,7 @@ defmodule Database do
 use GenServer
 
   def start_link(string) do
-    GenServer.start_link(__MODULE__, string, name: __MODULE__)
+    GenServer.start_link(__MODULE__, string, name: __MODULE__, time: :os.system_time(:millisecond))
   end
 
   def init(_msg) do
@@ -17,18 +17,20 @@ use GenServer
     Mongo.insert_one!(pid, collection, string)
   end
  #adaptive batching with a batch size 128
+ 
   @impl true
-  def handle_cast({:data, item}, list) do
+  def handle_cast({:data, item}, list, state) do
+    window = :os.system_time(:millisecond)
     add_to_db = [item | list]
-    if Enum.count(add_to_db) >= 128 do
+    if Enum.count(add_to_db) >= 128  or window - state.time > 200 do
       GenServer.cast(__MODULE__, :batch)
     end
 
-    {:noreply, add_to_db}
+    {:noreply, add_to_db, time: :os.system_time(:millisecond)}
   end
 
   @impl true
-  def handle_cast(:batch, list) do
+  def handle_cast(:batch, list, state) do
     spawn(fn ->
       Enum.each(list, fn item ->
         if Map.equal?(Map.take(item, ["protected"]),%{"protected" => false}) do
@@ -38,6 +40,6 @@ use GenServer
         end
       end)
     end)
-    {:noreply, []}
+    {:noreply, [], time: state.time}
   end
 end
